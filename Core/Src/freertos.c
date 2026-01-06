2dd44fd4687d978d3e0daca250466ffd524a1165
张亚飞 <1739077662@qq.com>
2026-01-07 02:01:10 +0800

    receive

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -1,29 +1,29 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * File Name          : freertos.c
-  * Description        : Code for freertos applications
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * File Name          : freertos.c
+ * Description        : Code for freertos applications
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
 #include "FreeRTOS.h"
+#include "cmsis_os.h"
+#include "main.h"
 #include "stm32f1xx.h"
 #include "stm32f1xx_hal_gpio.h"
 #include "task.h"
-#include "main.h"
-#include "cmsis_os.h"
 #include "tim.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -56,16 +56,16 @@
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
+    .name = "defaultTask",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityNormal,
 };
 /* Definitions for nrf24Task */
 osThreadId_t nrf24TaskHandle;
 const osThreadAttr_t nrf24Task_attributes = {
-  .name = "nrf24Task",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityLow,
+    .name = "nrf24Task",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityLow,
 };
 
 /* Private function prototypes -----------------------------------------------*/
@@ -79,20 +79,18 @@ void StartNrf24Task(void *argument);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /**
-  * @brief  FreeRTOS initialization
-  * @param  None
-  * @retval None
-  */
+ * @brief  FreeRTOS initialization
+ * @param  None
+ * @retval None
+ */
 void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN Init */
 
   // 电机舵机pwm初始化
-  HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_1);
-  HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
-  HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_3);
-  HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_4);
-
-
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4);
 
   /* USER CODE END Init */
 
@@ -114,7 +112,8 @@ void MX_FREERTOS_Init(void) {
 
   /* Create the thread(s) */
   /* creation of defaultTask */
-  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+  defaultTaskHandle =
+      osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
 
   /* creation of nrf24Task */
   nrf24TaskHandle = osThreadNew(StartNrf24Task, NULL, &nrf24Task_attributes);
@@ -126,24 +125,21 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN RTOS_EVENTS */
   /* add events, ... */
   /* USER CODE END RTOS_EVENTS */
-
 }
 
 /* USER CODE BEGIN Header_StartDefaultTask */
 /**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
+ * @brief  Function implementing the defaultTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
-{
+void StartDefaultTask(void *argument) {
   /* USER CODE BEGIN StartDefaultTask */
   /* Infinite loop */
   (void)argument;
   // int pwmValue = 0;
-  for(;;)
-  {
+  for (;;) {
     osDelay(100);
     // HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
     // __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);
@@ -156,19 +152,18 @@ void StartDefaultTask(void *argument)
 
 /* USER CODE BEGIN Header_StartNrf24Task */
 /**
-* @brief Function implementing the nrf24Task thread.
-* @param argument: Not used
-* @retval None
-*/
+ * @brief Function implementing the nrf24Task thread.
+ * @param argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_StartNrf24Task */
-void StartNrf24Task(void *argument)
-{
+void StartNrf24Task(void *argument) {
   /* USER CODE BEGIN StartNrf24Task */
   /* Infinite loop */
   (void)argument;
-    // NRF24初始化
+  // NRF24初始化
   NRF24_Init(&hspi1);
-   // 检测NRF24L01是否存在
+  // 检测NRF24L01是否存在
   if (NRF24_Check() != 1) {
     // 未检测到NRF24L01，可以在这里添加错误处理
     while (1)
@@ -176,39 +171,58 @@ void StartNrf24Task(void *argument)
   }
   // 设置为接收模式
   NRF24_SetRxMode(nrf24_addr);
-    /**
-  x轴 舵机值 范围 50-150 对应 90度范围 ； 接收侧pwm分辨率2000
-  y轴上 范围0-100； 接收侧收到数据x10，因为pwm分辨率是1000
-  y轴下 范围0-100； 接收侧收到数据x10，因为pwm分辨率是1000
-  */
+  /**
+x轴 舵机值 范围 50-150 对应 90度范围 ； 接收侧pwm分辨率2000
+y轴上 范围0-100； 接收侧收到数据x10，因为pwm分辨率是1000
+y轴下 范围0-100； 接收侧收到数据x10，因为pwm分辨率是1000
+*/
   uint8_t rx_data[4];
   uint8_t rx_len = 0;
-  
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13,SET);
+
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, SET);
 
   uint32_t linking_timestamp = 0;
-  for(;;)
-  {
-       rx_len = NRF24_ReceiveData(rx_data, 4);
+  for (;;) {
+    rx_len = NRF24_ReceiveData(rx_data, 4);
     if (rx_len > 0) {
-      
-        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+
+      HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
       linking_timestamp = HAL_GetTick(); // 记录接收数据的时间戳
       // 接收到数据，通过串口发送出去
-      
-      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);
-      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, rx_data[0] * 10); // x轴舵机控制
+
+      // if (rx_data[0] < 100) {
+      //   __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);
+      //   __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2,
+      //                         (100 - rx_data[0]) * 10); // x轴舵机控制
+      // } else if (rx_data[0] > 100) {
+      //   __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, (rx_data[0] - 100) * 10);
+      //   __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0); // x轴舵机控制
+      // } else {
+
+      //   __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);
+      //   __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0); // x轴舵机控制
+      // }
+
+      if (rx_data[1] < 100) {
+        __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 0);
+        __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4,
+                              (100 - rx_data[1]) * 10); // y轴舵机控制
+      } else if (rx_data[1] > 100) {
+        __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, (rx_data[1] - 100) * 10);
+        __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 0); // y轴舵机控制
+      } else {
+
+        __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 0);
+        __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 0); // y轴舵机控制
+      }
+
       // 清空接收缓冲区
-      memset(rx_data, 0, 3);
+      memset(rx_data, 0, 4);
     } else {
       if (HAL_GetTick() - linking_timestamp > 1000) {
         // 重置舵机
-        __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, 100);
         // 超过1秒没有接收到数据，关闭LED指示灯
         // 重置电机
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_RESET);
-        __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1, 0);
       }
     }
     osDelay(100);
@@ -220,4 +234,3 @@ void StartNrf24Task(void *argument)
 /* USER CODE BEGIN Application */
 
 /* USER CODE END Application */
-
